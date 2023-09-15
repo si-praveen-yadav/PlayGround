bf28e0f5cdf789afdc49cc0637737a58bcf69663
Praveen Yadav <praveen.yadav@sportzinteractive.net>
2023-09-15 13:55:48 +0530

    new changes

--- app/src/main/java/com/example/playground/MainActivity.kt
+++ app/src/main/java/com/example/playground/MainActivity.kt
@@ -8,8 +8,10 @@ import android.text.SpannableString
 import android.text.TextWatcher
 import android.text.style.ForegroundColorSpan
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.playground.databinding.ActivityMainBinding
@@ -17,10 +19,14 @@ import com.example.playground.databinding.ItemUserBinding
 
 
 class MainActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val TAG = "MainActivity"
         const val TAG_TEST = "MainActivityTest"
+        const val beforeTextChanged = "beforeTextChanged"
+        const val onTextChanged = "onTextChanged"
+        const val afterTextChanged = "afterTextChanged"
     }
+
     private lateinit var binding: ActivityMainBinding
 
     private var messages = mutableListOf<Message>()
@@ -31,6 +37,7 @@ class MainActivity : AppCompatActivity() {
     private val viewRegex = "@\\{([A-Z][a-z])?\\w+(\\s([A-Z][a-z])?(\\w+)?)?\\}"
 
     private var displayString = ""
+    private var prevDisplayString = ""
     private var messageString = ""
     private var cursorPosition = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,43 +52,102 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupEditMessage() {
 
-        binding.etMessage.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                Log.i(TAG,"beforeTextChanged : ${p0.toString()}")
+        binding.etMessage.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                Log.i(TAG,"beforeTextChanged : ${p0.toString()}")
+                Log.i(beforeTextChanged, "beforeTextChanged : CharSequence > $s")
+                Log.i(beforeTextChanged, "beforeTextChanged : start > $start")
+                Log.i(beforeTextChanged, "beforeTextChanged : count > $count")
+                Log.i(beforeTextChanged, "beforeTextChanged : after > $after")
+                prevDisplayString = s.toString()
+
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                /*Log.i(TAG,"onTextChanged : CharSequence > $s")
-                Log.i(TAG,"onTextChanged : start > $start")
-                Log.i(TAG,"onTextChanged : before > $before")
-                Log.i(TAG,"onTextChanged : count > $count")
-                Log.i(TAG,"onTextChanged : cursor position > ${binding.etMessage.selectionStart}")
-                */
-                Log.i(TAG,"onTextChanged : ${s.toString()}")
-//                Log.i(TAG,"onTextChanged : Mentioned Users > ${findMentionedUsers(s.toString())}")
+                Log.i(onTextChanged, "onTextChanged : CharSequence > $s")
+                Log.i(onTextChanged, "onTextChanged : start > $start")
+                Log.i(onTextChanged, "onTextChanged : before > $before")
+                Log.i(onTextChanged, "onTextChanged : count > $count")
+                updateMessageString(prevDisplayString,s.toString(),start,count,before)
+
+                /*Log.i(TAG,"onTextChanged : ${s.toString()}")
+                Log.i(TAG,"onTextChanged : Mentioned Users > ${findMentionedUsers(s.toString())}")
 
                 cursorPosition = binding.etMessage.selectionStart
+                updateMessageString(s.toString(),cursorPosition)
                 messageString = s.toString()
                 if(!s.isNullOrEmpty()){
                     filterMatchedUsers(findMentionedUsers(messageString.substring(0,cursorPosition)))
-                }
+                }*/
 
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                Log.i(TAG,"afterTextChanged : ${p0.toString()}")
+                Log.i(afterTextChanged, "afterTextChanged : ${p0.toString()}")
             }
         })
     }
 
-    private fun filterMatchedUsers(matches:List<String>){
+    private fun updateMessageString(prevText:String,currentText: String, i: Int,addedChars:Int,modifiedChars:Int) {
+        Log.i(TAG_TEST, "text -> $prevText")
+        Log.i(TAG_TEST, "cursorPosition -> $i")
+        val current = currentText.toMutableList()
+        val prev = messageString.toMutableList()
+
+        val spans = fetchViewMentionedUsers(messageString)
+        val coloredSpans = mutableListOf<Pair<Int, Int>>()
+        spans.forEach {
+            val start = messageString.indexOf(it)
+            val end = start + it.length
+
+            coloredSpans.add(Pair(start, end))
+        }
+
+        // check if editing done in mentions
+        var editedSpans = mutableListOf<Pair<Int, Int>>()
+        coloredSpans.forEach {
+            if (i in it.first..it.second) {
+                editedSpans.add(Pair(it.first, it.second))
+            }
+
+        }
+
+        editedSpans.forEach {
+            prev.removeAt(it.first + 1)
+            prev.removeAt(it.second)
+        }
+
+
+
+
+
+        /*if(prev.size<i){
+            prev.add(current[i])
+        }else{
+            prev.add(i,current[i])
+        }*/
+
+        Log.i(TAG_TEST, "prev message string -> $prev")
+        Log.i(TAG_TEST, "last messageString -> $messageString")
+
+        messageString = ""
+
+        Log.i(TAG_TEST, "updated messageString -> $messageString")
+    }
+
+    private fun filterMatchedUsers(matches: List<String>) {
         val filteredData = mutableListOf<User>()
         val fMatches = matches.map {
             it.substring(1)
         }
 
-        users.forEach {user->
-            if(fMatches.isNotEmpty() && user.name.startsWith(fMatches.last(), ignoreCase = true)) filteredData.add(user)
+        users.forEach { user ->
+            if (fMatches.isNotEmpty() && user.name.startsWith(
+                    fMatches.last(),
+                    ignoreCase = true
+                )
+            ) filteredData.add(user)
         }
 
         rvUsersAdapter.submitList(filteredData)
@@ -99,12 +165,12 @@ class MainActivity : AppCompatActivity() {
         return mentions.map { it.value }.toList()
     }
 
-    private fun setEditTextMentions(user: User){
+    private fun setEditTextMentions(user: User) {
         val selectedUser = user.name
         val currentText = binding.etMessage.text.toString()
         val mentionText = "@{$selectedUser} "
 
-        val mentionStartIndex = currentText.substring(0,cursorPosition).lastIndexOf("@")
+        val mentionStartIndex = currentText.substring(0, cursorPosition).lastIndexOf("@")
         val newText =
             currentText.replaceRange(
                 mentionStartIndex,
@@ -115,12 +181,12 @@ class MainActivity : AppCompatActivity() {
         messageString = newText
 
         val spans = fetchViewMentionedUsers(newText)
-        val coloredSpans = mutableListOf<Pair<Int,Int>>()
+        val coloredSpans = mutableListOf<Pair<Int, Int>>()
         spans.forEach {
             val start = newText.indexOf(it)
             val end = start + it.length
 
-            coloredSpans.add(Pair(start,end))
+            coloredSpans.add(Pair(start, end))
         }
 
         val mySpan = SpannableString(newText)
@@ -156,6 +222,7 @@ class MainActivity : AppCompatActivity() {
         )
         return spannable
     }
+
     private fun setupRvUsers() {
         binding.rvUsers.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
